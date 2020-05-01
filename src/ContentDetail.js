f730e25f707da9cd5f6fcfb95a215c6f775c79a9
lilalur <petercselik@gmail.com>
2020-05-01 16:11:34 +0100

    style update

--- src/ContentDetail.js
+++ src/ContentDetail.js
@@ -27,32 +27,10 @@ export default function ContentDetail( {match} ) {
     };
 
     return (
-        // <div>
-        //     <h2>{item.name}</h2>
-        //     <img src={item.img} alt={"Image of " + item.name} />
-        //     <p>{item.faction}</p>
-        //     <p>{item.playerClass}</p>
-        //     <p>{item.rarity}</p>
-        //     <p>{item.type}</p>
-        // </div>
-
-
-        // <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        //     <div className="col p-4 d-flex flex-column position-static">
-        //         <strong className="d-inline-block mb-2 text-primary">World</strong>
-        //         <h3 className="mb-0">Featured post</h3>
-        //         <div className="mb-1 text-muted">Nov 12</div>
-        //         <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-        //         <a href="#" className="stretched-link">Continue reading</a>
-        //     </div>
-        //     <div className="col-auto d-none d-lg-block">
-        //         <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        //     </div>
-        // </div>
 
         <div className="row align-items-center border-box-udnerit">
           <div className="col-lg-6 col-md-5 col-12 order-md-2 flex-container-center">
-            <img src={item.img} className="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0 aos-init aos-animate" alt={"Image of " + item.name} data-aos="fade-up" data-aos-delay="100" />
+            <img src={item.imgGold} className="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0 aos-init aos-animate" alt={"Image of " + item.name} data-aos="fade-up" data-aos-delay="100" />
           </div>
           <div className="col-lg-6 col-md-7 col-12 order-md-1 aos-init aos-animate" data-aos="fade-up">
 
@@ -60,7 +38,7 @@ export default function ContentDetail( {match} ) {
               The card of <span className="text-primary">{item.name}</span> <br /> 
             </h1>
             
-            <p>{item.rarity} | {item.faction} | {item.playerClass} | {item.type}</p>
+            <p className="text-center text-md-left">{item.rarity} | {item.faction} | {item.playerClass} | {item.type}</p>
           </div>
         </div>
 
