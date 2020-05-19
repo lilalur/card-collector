6004d1f572348f873d8265abd9599c49e672764c
lilalur <petercselik@gmail.com>
2020-05-19 10:37:16 +0100

    Updated the loader animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,34 +1,20 @@
 import React from 'react'
 
 export const Loader = () => {
+    let cards = [];
+    for(let i = 0; i < 5; i++) {
+        cards.push(
+            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 loader--dot" key={i+"loadercard"}>
+                <div className="card mb-3 scale-up-swing">
+                        <img className="shadowed grow card-img-top" src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png"  />
+                </div>
+            </div>
+        );
+    }
 
     return (
-        <div class='card-loader'>
-            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 loader--dot">
-                <div className="card mb-3 scale-up-swing">
-                        <img className="shadowed grow card-img-top" src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png"  />
-                </div>
-            </div> 
-            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 loader--dot">
-                <div className="card mb-3 scale-up-swing">
-                        <img className="shadowed grow card-img-top" src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png"  />
-                </div>
-            </div> 
-            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 loader--dot">
-                <div className="card mb-3 scale-up-swing">
-                        <img className="shadowed grow card-img-top" src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png"  />
-                </div>
-            </div> 
-            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 loader--dot">
-                <div className="card mb-3 scale-up-swing">
-                        <img className="shadowed grow card-img-top" src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png"  />
-                </div>
-            </div> 
-            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 loader--dot">
-                <div className="card mb-3 scale-up-swing">
-                        <img className="shadowed grow card-img-top" src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png"  />
-                </div>
-            </div> 
+        <div className='card-loader'>
+        {cards}
         </div>
     )
 }
